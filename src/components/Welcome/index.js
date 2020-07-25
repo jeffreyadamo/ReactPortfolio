36cc184c3dbf634d0fb95e915932cb553da875be
jeffreyadamo <jeffreyadamo@gmail.com>
2020-07-25 16:59:28 -0700

    reorg files, css updates

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../../index.css";
+
+function Welcome() {
+  return (
+    <div>
+      <div className="card-panel cyan darken-1 aboutText">
+        <h4 className="center">Welcome to my web development portfolio!</h4>
+        <br />
+        Here, you'll see the various projects I've completed while enrolled in
+        UW's Full Stack Web Development BootCamp.
+        <div>(Feb 2020 - present)</div>
+        <br />
+      </div>{" "}
+      <br /> <br /> <br /> <br />
+    </div>
+  );
+}
+
+export default Welcome;
